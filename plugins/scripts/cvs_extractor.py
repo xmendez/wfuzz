264087fe4dc3c2e3dd94f12945cb89d9413bdb36
xmendez <xmendez@edge-security.com>
2015-03-25 19:59:45 +0000

    CVS entries extractor plugin

---
+++ plugins/scripts/cvs_extractor.py
@@ -0,0 +1,44 @@
+from urlparse import urlparse, urljoin
+
+from framework.plugins.base import DiscoveryPlugin
+from framework.core.myexception import FuzzException
+from externals.moduleman.plugin import moduleman_plugin
+
+import tempfile
+import sqlite3
+
+# Entries format based on:
+# http://docstore.mik.ua/orelly/other/cvs/cvs-CHP-6-SECT-9.htm
+# Good example at http://webscantest.com/CVS/Entries 
+
+@moduleman_plugin
+class cvs_extractor(DiscoveryPlugin):
+    name = "cvs_extractor"
+    description = "Parses CVS/Entries file. Optional: discovery.bl=\".txt,.gif\""
+    category = ["default", "active", "discovery"]
+    priority = 99
+
+    def validate(self, fuzzresult):
+	ctype = None
+	if fuzzresult.history.fr_headers()['response'].has_key('Content-Type'):
+	    ctype = fuzzresult.history.fr_headers()['response']['Content-Type']
+
+	return fuzzresult.url.find("CVS/Entries") >= 0 and fuzzresult.code == 200 and (not ctype or (ctype and any(map(lambda x: ctype.find(x) >= 0, ['text/plain']))))
+
+    def process(self, fuzzresult):
+	base_url = urljoin(fuzzresult.url, "..")
+
+	for line in fuzzresult.history.fr_content().splitlines():
+	    record = line.split("/")
+	    if len(record) == 6 and record[1]:
+		    u = urljoin(base_url, record[1])
+		    if not self.blacklisted_extension(u):
+			self.queue_url(u)
+
+		    # Directory
+		    if record[0] == 'D':
+			u = urljoin(base_url, record[1])
+			self.queue_url(u)
+
+			u = urljoin(base_url, "%s/CVS/Entries" % (record[1]))
+			self.queue_url(u)
