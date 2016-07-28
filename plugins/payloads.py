ca47b4182ead9eceb10f41094cca3af2431c29c9
javi <xmendez@edge-security.com>
2016-07-28 01:36:58 +0100

    ipnet plugin

--- plugins/payloads.py
+++ plugins/payloads.py
@@ -9,6 +9,39 @@ from framework.plugins.api import search_bing
 
 @wfuzz_iterator
 @moduleman_plugin("count", "next", "__iter__")
+class ipnet:
+    name = "ipnet"
+    description = "Returns list of IP addresses of a given network. ie. 192.168.1.0/24"
+    category = ["default"]
+    priority = 99
+
+    def __init__(self, network):
+	try:
+            import ipaddress
+
+            net = ipaddress.ip_network(u'%s' % network)
+            self.f = net.hosts()
+            self.__count = net.num_addresses - 2
+
+            if self.__count <= 0:
+                raise FuzzException(FuzzException.FATAL, "There are not hosts in the specified network")
+
+	except ValueError:
+	    raise FuzzException(FuzzException.FATAL, "The specified network has an incorrect format.")
+	except ImportError:
+	    raise FuzzException(FuzzException.FATAL, "ipnet plugin requires ipaddress module. Please install it using pip.")
+
+    def next(self):
+	return str(self.f.next())
+
+    def count(self):
+	return self.__count
+
+    def __iter__ (self):
+	return self
+
+@wfuzz_iterator
+@moduleman_plugin("count", "next", "__iter__")
 class file:
     name = "file"
     description = "Returns each word from a file."
