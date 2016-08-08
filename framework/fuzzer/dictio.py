xmendez/wfuzz

commit e55b1b6e476d0c7d14c0661f745a95c5eb00e04c

baseline is generated in genreq (fixes #417)

framework/fuzzer/dictio.py
@@ -88,6 +88,9 @@ class requestGenerator:
 		    yield r
 
 	def next(self):
+	    if self._baseline and self.stats.processed == 0 and self.stats.pending_seeds <= 1:
+		return self._baseline
+
 	    if self.stats.cancelled:
 		raise StopIteration
 
@@ -96,6 +99,3 @@ class requestGenerator:
 	    else:
 		n = self.dictio.next()
 		return FuzzRequest.from_seed(self.seed, (n,) if isinstance(n, str) else n)
-
-	def get_baseline(self):
-	    return self._baseline

framework/fuzzer/Fuzzer.py
@@ -35,22 +35,9 @@ class SeedQ(FuzzQueue):
     def _cleanup(self):
 	pass
 
-    def do_baseline(self):
-	# initial baseline request
-	bl = self.genReq.get_baseline()
-	if bl:
-	    self.genReq.stats.pending_fuzz += 1
-	    self.queue_out.put_first(bl)
-
-	    # wait for BBB to be completed before generating more items
-	    while(self.genReq.stats.processed == 0 and not self.genReq.stats.cancelled):
-		time.sleep(0.0001)
-
     def process(self, prio, item):
 	if isinstance(item, requestGenerator):
 	    self.genReq.stats.pending_seeds += 1
-	    if self.genReq.stats.pending_seeds == 1:
-		self.do_baseline()
 	elif isinstance(item, FuzzRequest):
 	    self.genReq.restart(item)
 	else:
@@ -59,6 +46,18 @@ class SeedQ(FuzzQueue):
 	# Empty dictionary?
 	try:
 	    rq = self.genReq.next()
+
+	    if rq.wf_is_baseline:
+		self.genReq.stats.pending_fuzz += 1
+		self.queue_out.put_first(rq)
+
+		# wait for BBB to be completed before generating more items
+		while(self.genReq.stats.processed == 0 and not self.genReq.stats.cancelled):
+		    time.sleep(0.0001)
+
+		# more after baseline?
+		rq = self.genReq.next()
+
 	except StopIteration:
 	    raise FuzzException(FuzzException.FATAL, "Empty dictionary! Please check payload or filter.")
 

commit 56c585fd9a53fa56443115ddf97547c6f666a2de

fixed bug introduced in e55b1b6e476d0c7d14c0661f745a95c5eb00e04c where genreq does not stop generating baseline (fixes #440)

framework/fuzzer/dictio.py
@@ -89,11 +89,11 @@ class requestGenerator:
 		    yield r
 
 	def next(self):
-	    if self._baseline and self.stats.processed == 0 and self.stats.pending_seeds <= 1:
-		return self._baseline
-
 	    if self.stats.cancelled:
 		raise StopIteration
+
+	    if self._baseline and self.stats.processed == 0 and self.stats.pending_seeds <= 1:
+		return self._baseline
 
 	    if self.seed.history.wf_allvars is not None:
 		return self._allvar_gen.next()