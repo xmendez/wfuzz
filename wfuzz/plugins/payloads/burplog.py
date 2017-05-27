0ca82ef6dbf0f41684c1f79b2aec7414dc5c1818
javi <xmendez@edge-security.com>
2017-05-27 17:23:35 +0200

    burplog faster and mem efficient sequential file read (fixes #586)

--- wfuzz/plugins/payloads/burplog.py
+++ wfuzz/plugins/payloads/burplog.py
@@ -3,9 +3,6 @@ from wfuzz.exception import FuzzExceptBadFile
 from wfuzz.fuzzobjects import FuzzResult, FuzzRequest
 from wfuzz.plugin_api.base import BasePayload
 
-import datetime
-import logging
-import os
 import re
 
 CRLF = "\r\n"
@@ -36,7 +33,7 @@ class burplog(BasePayload):
 
 	self.__max = -1
         self.attr = self.params["attr"]
-        self._it = self.parse(self.open_burp_log(self.params["fn"]))
+        self._it = self.parse_burp_log(self.params["fn"])
 
     def __iter__(self):
 	return self
@@ -49,153 +46,63 @@ class burplog(BasePayload):
 
         return next_item if not self.attr else next_item.get_field(self.attr)
 
-    def open_burp_log(self, burp_log):
+    def parse_burp_log(self, burp_log):
         burp_file = None
 
         try:
             burp_file = open(self.find_file(burp_log), 'rb')
-            buf = burp_file.read()
+
+            history = 'START'
+
+            rl = burp_file.readline()
+            while rl != "":
+                if history == "START":
+                    if rl == DELIMITER:
+                        history = "HEADER"
+                elif history == "HEADER":
+                    if rl == DELIMITER:
+                        raw_request = ""
+                        history = "REQUEST"
+                    else:
+                        matched = HEADER.match(rl)
+                        ctime, host, ip_address = matched.group(1, 3, 5)
+                elif history == "REQUEST":
+                    if rl == DELIMITER:
+                        history = "DELIM1"
+                    else:
+                        raw_request += rl
+                elif history == "DELIM1":
+                    if rl == CRLF:
+                        raw_response = ""
+                        history = "DELIM3"
+                    else:
+                        raw_response = rl
+                        history = "RESPONSE"
+                elif history == "RESPONSE":
+                    if rl == DELIMITER:
+                        history = "DELIM2"
+                    else:
+                        raw_response += rl
+                elif history == "DELIM2":
+                    if rl == CRLF:
+                        history = "DELIM3"
+                elif history == "DELIM3":
+                    if rl == CRLF:
+                        history = "DELIM4"
+                elif history == "DELIM4":
+                    if rl == CRLF:
+                        fr = FuzzRequest()
+                        fr.update_from_raw_http(raw_request, host[:host.find("://")], raw_response)
+                        frr = FuzzResult(history=fr)
+
+                        yield frr.update()
+
+                        history = "START"
+
+                rl = burp_file.readline()
 
         except IOError, e:
             raise FuzzExceptBadFile("Error opening burp log file. %s" % str(e))
         finally:
             if burp_file is not None:
                 burp_file.close()
-
-        return buf
-
-    def forward_buffer(self, buf, pos, n, token):
-        """
-        Advance buf from current position by n bytes while buf is not equal
-        to token.
-
-        @param buffer: String
-        @param pos: Integer of current position in buffer.
-        @param n: Length of token string.
-        @param token: Token to advance current buffer position to.
-        @return: Position of buffer at token.
-        @rtype: int
-        """
-        while buf[pos:pos + n] != token:
-            pos += 1
-
-        return pos
-
-    def parse(self, buf):
-        """
-        Parses a Burp Suite log file.  Returns a list of Burp objects
-        in the order in which they were written.
-
-        @param burp_log: A filename or string of a Burp Suite log.
-        @return: list of gds.burp.Burp objects.
-        @rtype: list
-        """
-
-        parsed = []
-        history = 'START'
-
-        pos = 0
-        req = 0
-
-        buf_len = len(buf)
-
-        while pos < buf_len + 1:
-            try:
-                if history == "START":
-                    if buf[pos:pos + 56] == DELIMITER:
-                        history = "HEADER"
-                    else:
-                        pos += 1
-
-                # Parse the header lines
-                if history == "HEADER":
-                    start = pos
-
-                    # First check to make sure we've got a header block
-                    pos += 56
-                    pos = self.forward_buffer(buf, pos, 2, CRLF)
-
-                    header = buf[start + 56:pos]
-
-                    # Advance over CRLF
-                    pos += 2
-
-                    if buf[start:start + 56] == DELIMITER and \
-                        buf[pos:pos + 56] == DELIMITER:
-
-                        # we are positive this is a header and not just a
-                        # coincidence that the delimiter was in the body.
-
-                        matched = HEADER.match(header)
-                        ctime, host, ip_address = matched.group(1, 3, 5)
-                        history = "REQUEST"
-                    else:
-                        history = "START"
-
-                elif history == "REQUEST":
-                    start = pos
-                    pos += 56
-                    start2 = pos
-                    pos = self.forward_buffer(buf, pos, 2, CRLF)
-
-
-                    pos = self.forward_buffer(buf, pos, 4, CRLF + CRLF)
-
-                    # Advance over CRLF
-                    pos += 4
-                    start = pos
-                    pos = self.forward_buffer(buf, pos, 56, DELIMITER)
-
-                    # at this point, we're right at the delimiter, so -2 bytes
-                    # to account for that last CRLF.
-                    raw_request = buf[start2:pos - 2]
-
-                    # we got the body, now advance over the delimiter
-                    pos += 56
-
-                    history = "RESPONSE"
-
-                if history == "RESPONSE":
-                    start = pos
-
-                    pos = self.forward_buffer(buf, pos, 2, CRLF)
-
-                    # slice buf from index of current position + 3 CRLF
-                    # to current position + 3 CRLF + delimiter length (= 62)
-                    if buf[pos + 6:pos + 62] != DELIMITER:
-
-                        pos = self.forward_buffer(buf, pos, 4, CRLF + CRLF)
-
-                        # Advance over CRLF
-                        pos += 4
-
-                        while buf[pos - 2:pos + 56] != CRLF_DELIMITER and \
-                            pos < buf_len - 2:
-                            pos += 1
-
-                        raw_response = buf[start:pos]
-
-                        pos += 56
-
-                    else:
-                        raw_response = ""
-
-                    fr = FuzzRequest()
-                    fr.update_from_raw_http(raw_request, host[:host.find("://")], raw_response)
-                    frr = FuzzResult(history=fr)
-                    
-
-                    yield frr.update()
-
-                    history = "START"
-
-            # The most likely cause for an exception to get raised is if
-            # modifications were made to the main Burp class and weren't handled
-            # correctly.  Check your source!
-            #
-            # If this is a legit exception due to incorrect parsing, please send
-            # labs@gdssecurity.com an email with the error message and if possible
-            # a sanitized proxy log.
-            except Exception, e:
-                print e
-                pos += 1
