01915c38bf7052482698f845e49de6d013e3e773
javi <xmendez@edge-security.com>
2017-10-14 20:50:05 +0200

    errors plugin

--- wfuzz/plugins/scripts/errors.py
+++ wfuzz/plugins/scripts/errors.py
@@ -23,14 +23,26 @@ class errors(BasePlugin):
 	    ('Apache Tomcat', 'Apache Tomcat/(.*?) - Error report'),
 	]
 
-	self.regex = []
+	self.server_regex = []
 	for server_name, i in regex:
-	    self.regex.append((server_name, re.compile(i, re.MULTILINE|re.DOTALL)))
+	    self.server_regex.append((server_name, re.compile(i, re.MULTILINE|re.DOTALL)))
+
+	regex = [
+            ('MySQL', 'You have an error in your SQL syntax'),
+	]
+
+	self.error_regex = []
+	for server_name, i in regex:
+	    self.error_regex.append((server_name, re.compile(i, re.MULTILINE|re.DOTALL)))
 
     def validate(self, fuzzresult):
 	return True
 
     def process(self, fuzzresult):
-	for server_name, r in self.regex:
+	for server_name, r in self.server_regex:
 	    for i in r.findall(fuzzresult.history.content):
 		self.add_result("Server error identified, version: %s %s" % (server_name, i))
+
+	for name, r in self.error_regex:
+	    for i in r.findall(fuzzresult.history.content):
+		self.add_result("%s error identified" % (name))
