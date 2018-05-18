670befa2035e73c3b9fbd4f75a678c2473f04516
javi <xmendez@edge-security.com>
2018-05-18 16:16:19 +0200

    setup: added classifiers and python_requres

--- setup.py
+++ setup.py
@@ -42,9 +42,21 @@ setup(
     description="Wfuzz - The web fuzzer",
     long_description=long_descr,
     author="Xavi Mendez (@x4vi_mendez)",
+    author_email="xmendez@edge-security.com"
     url="http://wfuzz.org",
     install_requires=install_requires,
     extras_require={
         'dev': dev_requires,
-    }
+    },
+    python_requires=">=2.6",
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ),
 )
