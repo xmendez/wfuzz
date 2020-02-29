247111008c4aef8e4f9a379ae021ecef620dedbc
Sergio Garcia Prado <sergio@garciparedes.me>
2020-02-29 14:20:41 +0100

    Revert "Removed warning related with "collections.abc" warning"
    This reverts commit c5da11ef

--- src/wfuzz/options.py
+++ src/wfuzz/options.py
@@ -18,7 +18,7 @@ from collections import defaultdict
 
 # python 2 and 3
 try:
-    from collections.abc import UserDict
+    from collections import UserDict
 except ImportError:
     from UserDict import UserDict
 
