d2d4e7d6ffad98407b1bc3be056922a2cf9b2a87
javi <xmendez@edge-security.com>
2020-05-09 13:30:41 +0200

    test py2 build in travis

--- tests/test_acceptance.py
+++ tests/test_acceptance.py
@@ -470,7 +470,7 @@ def create_tests():
         create_tests_from_list(testing_tests)
         duplicate_tests(testing_tests, "recipe", wfuzz_me_test_generator_recipe)
         duplicate_tests(testing_tests, "saveres", wfuzz_me_test_generator_saveres)
-        duplicate_tests_diff_params(testing_tests, "_proxy_", dict(proxies=[("localhost", 8080, "HTTP")]), None)
+        duplicate_tests_diff_params(testing_tests, "_proxy_", dict(proxies=[("mitmproxy", 8080, "HTTP")]), None)
     else:
         # this are the basics
         basic_functioning_tests = [error_tests, scanmode_tests, basic_tests]
