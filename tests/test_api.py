97c659499c3384f3bafa3d7a5ed16cf1668b82a4
javi <xmendez@edge-security.com>
2020-05-25 20:36:54 +0200

    move encoder test to pytest

--- tests/test_api.py
+++ tests/test_api.py
@@ -32,34 +32,6 @@ class APITests(unittest.TestCase):
         payload_list = wfuzz.get_payload([list(range(2)), list(range(2))]).data.get('dictio')[0]
         self.assertEqual(sorted(payload_list), sorted([[0, 1], [0, 1]]))
 
-    def test_encoders(self):
-        encoders = {
-            'none': ('test', 'test'),
-            'urlencode': ("../=?&", '../%3D%3F%26'),
-            'double_urlencode': ("../=?&", '../%253D%253F%2526'),
-            'double_urlencode': ("../=?&", '../%253D%253F%2526'),
-            'base64': ('admin', 'YWRtaW4='),
-
-            'sha1': ('admin', 'd033e22ae348aeb5660fc2140aec35850c4da997'),
-            'md5': ('admin', '21232f297a57a5a743894a0e4a801fc3'),
-            'hexlify': ('admin', '61646d696e'),
-            'html_escape': ('<>&\'"/', "&lt;&gt;&amp;&#x27;&quot;/"),
-            'html_decimal': ('<>&\'"/', '&#60;&#62;&#38;&#39;&#34;&#47;'),
-            'html_hexadecimal': ('<>&\'"/', '&#x3c;&#x3e;&#x26;&#x27;&#x22;&#x2f;'),
-
-            'mysql_char': ('admin', 'CHAR(97,100,109,105,110)'),
-            'mssql_char': ('admin', 'CHAR(97)+CHAR(100)+CHAR(109)+CHAR(105)+CHAR(110)'),
-            'oracle_char': ('admin', 'chr(97)||chr(100)||chr(109)||chr(105)||chr(110)'),
-        }
-
-        for key, values in list(encoders.items()):
-            value, expected = values
-            self.assertEqual(wfuzz.encode(key, value), expected)
-
-        for key, values in [items for items in list(encoders.items()) if items[0] not in ['html_hexadecimal', 'html_decimal', 'md5', 'sha1', 'html_escape']]:
-            value, expected = values
-            self.assertEqual(wfuzz.decode(key, expected), value)
-
     def test_decode(self):
         payload_list = wfuzz.get_payload([list(range(2)), list(range(2))]).data.get('dictio')[0]
         self.assertEqual(sorted(payload_list), sorted([[0, 1], [0, 1]]))
