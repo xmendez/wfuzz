2ff9b81609bb3865beab6cee2afee28f211d494d
javi <xmendez@edge-security.com>
2020-06-14 00:11:54 +0200

    add dotdict test

--- tests/helpers/test_dotdict.py
+++ tests/helpers/test_dotdict.py
@@ -15,6 +15,7 @@ def dotdict_ex2():
 
 
 def test_operators(dotdict_ex1, dotdict_ex2):
+    assert dotdict_ex1 == {"a": "1"}
     assert dotdict_ex1 + "test" == {"a": "1test"}
     assert "test" + dotdict_ex1 == {"a": "test1"}
     assert dotdict_ex1 + dotdict_ex2 == {"a": "2"}
