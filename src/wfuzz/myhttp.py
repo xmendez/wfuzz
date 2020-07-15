9cf184885f23f4ff4adda0a030910f5037112939
jasantiago-stratio <jasantiago@stratio.com>
2020-07-15 09:46:49 +0200

    Mutual TLS with --clientcert and --clientcertkey arguments

--- src/wfuzz/myhttp.py
+++ src/wfuzz/myhttp.py
@@ -152,6 +152,15 @@ class HttpPool:
         if cdelay is not None:
             c.setopt(pycurl.CONNECTTIMEOUT, cdelay)
 
+        clientcert = self.options.get("clientcert")
+        if clientcert is not None:
+            print("OU YEAH MAMMA!! "+clientcert)
+            c.setopt(pycurl.SSLCERT, clientcert)
+
+        clientcertkey = self.options.get("clientcertkey")
+        if clientcertkey is not None:
+            c.setopt(pycurl.SSLKEY, clientcertkey)
+
         return c
 
     def _process_curl_handle(self, curl_h):
