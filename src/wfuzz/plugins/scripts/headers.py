978a12ca46f5d1105375f031d05ee2eb9203a219
javi <xmendez@edge-security.com>
2020-10-02 22:04:34 +0200

    minor change to headers plugin

--- src/wfuzz/plugins/scripts/headers.py
+++ src/wfuzz/plugins/scripts/headers.py
@@ -2,7 +2,7 @@ from wfuzz.plugin_api.base import BasePlugin
 from wfuzz.externals.moduleman.plugin import moduleman_plugin
 
 
-KBASE_KEY = "http.headers.servers"
+KBASE_KEY = "http.response.headers.servers"
 
 
 @moduleman_plugin
@@ -10,7 +10,7 @@ class headers(BasePlugin):
     name = "headers"
     author = ("Xavi Mendez (@xmendez)",)
     version = "0.1"
-    summary = "Looks for new headers associated to HTTP servers in HTTP responses"
+    summary = "Looks for new response headers associated to HTTP servers in HTTP responses"
     description = ("Looks for new server headers",)
     category = ["info", "passive", "default"]
     priority = 99
@@ -19,23 +19,24 @@ class headers(BasePlugin):
     def __init__(self):
         BasePlugin.__init__(self)
 
+        self.watch_headers = [
+            "Server",
+            "X-Powered-By"
+            "Via"
+        ]
+
     def validate(self, fuzzresult):
         return True
 
     def process(self, fuzzresult):
 
-        watch_headers = [
-            "Server",
-            "X-Powered-By"
-        ]
-
-        for header in watch_headers:
+        for header in self.watch_headers:
             header_value = None
             if header in fuzzresult.history.headers.response:
                 header_value = fuzzresult.history.headers.response[header]
 
             if header_value is not None:
                 if header_value.lower() not in self.kbase[KBASE_KEY] or KBASE_KEY not in self.kbase:
-                    self.add_result("New server header. {}: {}".format(header, header_value))
+                    self.add_result("New server response header. {}: {}".format(header, header_value))
 
                     self.kbase[KBASE_KEY].append(header_value.lower())
