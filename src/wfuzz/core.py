0e74b475643b92455862e220e67ad5d92e12d901
javi <xmendez@edge-security.com>
2020-10-13 13:53:21 +0200

    only add routingq when transport is http

--- src/wfuzz/core.py
+++ src/wfuzz/core.py
@@ -59,7 +59,9 @@ class Fuzzer(object):
         if options.get("rlevel") > 0:
             self.qmanager.add("recursive_queue", RecursiveQ(options))
 
-        if (options.get("script") or options.get("rlevel") > 0) and options.get("transport") == "http":
+        if (options.get("script") or options.get("rlevel") > 0) and options.get(
+            "transport"
+        ) == "http":
             rq = RoutingQ(
                 options,
                 {
